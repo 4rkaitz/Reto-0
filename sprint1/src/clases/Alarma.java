78d63e7ba8bab4279c3f1edba39df99877c17b21
ik_2dm3 <ik_2dm3@212-TX200716.H015110.NET>
2020-09-17 09:54:27 +0200

    probando

---
+++ sprint1/src/clases/Alarma.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Alarma 
+{	
+	String nombreAula;
+
+	public Alarma(String nombreAula) 
+	{
+		this.nombreAula = nombreAula;
+	}
+
+	public String getNombreAula() 
+	{
+		return nombreAula;
+	}
+}
