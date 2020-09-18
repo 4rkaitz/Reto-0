f521108849fd9cfe6e1c1963582c06239b4c6196
ik_2dm3 <ik_2dm3@212-TX200722.H015110.NET>
2020-09-18 10:39:17 +0200

    Cambios en la estructura de carpetas

---
+++ Reto_0/src/clases/Calefactor.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Calefactor 
+{
+	String nombreAula;
+
+	public Calefactor(String nombreAula) 
+	{
+		this.nombreAula = nombreAula;
+	}
+
+	public String getNombreAula() 
+	{
+		return nombreAula;
+	}
+}
