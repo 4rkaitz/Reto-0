029828aab20b50f4ae130964be6670971a427002
ik_2dm3 <ik_2dm3@212-TX200716.H015110.NET>
2020-09-18 08:23:32 +0200

    up

--- sprint1/src/clases/Control.java
+++ sprint1/src/clases/Control.java
@@ -1,7 +1,7 @@
 package clases;
 
-public class Control {
-
+public class Control 
+{
 	public static void main(String[] args) 
 	{
 		//Inicialización de array de alarmas (101 - 116, 201 - 216 ...)
